32a56a5804cf62188b2a2c0d8005d3c4c5eb3cdf
pingfury <pingfury@outlook.com>
2024-12-22 22:47:04 +0800

    add content

--- background.ts
+++ background.ts
@@ -1,1 +1,9 @@
 console.log('Hello from the background script!');
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'document') {
+    const data = request.data;
+    console.log(data);
+  }
+});
